319970af3d74428325be05b20f403363a012b456
MartinMinuto <martin.minuto1@gmail.com>
2022-11-18 17:48:47 -0300

    Entrega3.0

--- js/Categorias.js/ArrayProcesadores.js
+++ js/Categorias.js/ArrayProcesadores.js
@@ -1,89 +1,133 @@
+const carrito = [];
+
+
 
 const Procesadores = [
-   {
-    id:1,
-    nombre:"I3 9gen",
-    marca:"Intel",
-    precio:"$900",
+  {
+   id:1,
+   nombre:"I3 9gen",
+   marca:"Intel",
+   precio:"$900",
+   stock:"Sin stock", // De momento
+   imagen:'./imagenes/intel.jpg'
+  },
+  {
+   id:2,
+   nombre:"I3 10gen",
+   marca:"Intel",
+   precio:"$950",
+   stock:"Sin stock", // De momento
+   imagen:'./imagenes/intel.jpg'
+  },
+  {
+   id:3,
+   nombre:"I3 11gen",
+   marca:"Intel",
+   precio:"$1100",
+   stock:"Sin stock", // De momento
+   imagen:'./imagenes/intel.jpg'
+  },
+  {
+   id:4,
+   nombre:"I5 9gen",
+   marca:"Intel",
+   precio:"$1050",
+   stock:"Sin stock",// De momento
+   imagen:'./imagenes/intel.jpg'
+  },
+  {
+   id:5,
+   nombre:"I5 10gen",
+   marca:"Intel",
+   precio:"$1140",
+   stock:"Sin stock", // De momento
+   imagen:'./imagenes/intel.jpg'
+  },
+  {
+   id:6,
+   nombre:"I5 11gen",
+   marca:"Intel",
+   precio:"$1350",
+   stock:"Sin stock", // De momento
+   imagen:'./imagenes/intel.jpg'
+  },
+  {
+    id:7,
+    nombre:"Ryzen 3",
+    marca:"AMD",
+    precio:"$890",
     stock:"Sin stock", // De momento
     imagen:'./imagenes/intel.jpg'
    },
    {
-    id:2,
-    nombre:"I3 10gen",
-    marca:"Intel",
-    precio:"$950",
+    id:8,
+    nombre:"Ryzen 5",
+    marca:"AMD",
+    precio:"$1250",
     stock:"Sin stock", // De momento
     imagen:'./imagenes/intel.jpg'
    },
    {
-    id:3,
-    nombre:"I3 11gen",
-    marca:"Intel",
-    precio:"$1100",
+    id:9,
+    nombre:"Ryzen 7",
+    marca:"AMD",
+    precio:"$1500",
     stock:"Sin stock", // De momento
     imagen:'./imagenes/intel.jpg'
-   },
-   {
-    id:4,
-    nombre:"I5 9gen",
-    marca:"Intel",
-    precio:"$1050",
-    stock:"Sin stock",// De momento
-    imagen:'./imagenes/intel.jpg'
-   },
-   {
-    id:5,
-    nombre:"I5 10gen",
-    marca:"Intel",
-    precio:"$1140",
-    stock:"Sin stock", // De momento
-    imagen:'./imagenes/intel.jpg'
-   },
-   {
-    id:6,
-    nombre:"I5 11gen",
-    marca:"Intel",
-    precio:"$1350",
-    stock:"Sin stock", // De momento
-    imagen:'./imagenes/intel.jpg'
-   },
-   {
-     id:7,
-     nombre:"Ryzen 3",
-     marca:"AMD",
-     precio:"$890",
-     stock:"Sin stock", // De momento
-     imagen:'./imagenes/intel.jpg'
-    },
-    {
-     id:8,
-     nombre:"Ryzen 5",
-     marca:"AMD",
-     precio:"$1250",
-     stock:"Sin stock", // De momento
-     imagen:'./imagenes/intel.jpg'
-    },
-    {
-     id:9,
-     nombre:"Ryzen 7",
-     marca:"AMD",
-     precio:"$1500",
-     stock:"Sin stock", // De momento
-     imagen:'./imagenes/intel.jpg'
-    }
+   }
 ];
 
+let ArticulosCard = document.getElementById('carta');
+function Productos(){
+for (const Procesador of Procesadores){
+  let card = document.createElement('div');
+  card.className ="card col-3"
+  card.innerHTML += `
+              <div class="card">
+                   <img src=../Imagenes/intel.jpg class="card-img-top" alt=Procesador>
+                         <div class="card-body">
+                            <h5 class="card-title">${Procesador.nombre}</h5>
+                            <p class="card-text">${Procesador.precio}</p>
+                            <button id='Boton${Procesador.id}' class="btn">Comprar</button> 
+                         </div>
+             </div>
+  
+  `;
+  ArticulosCard.append(card);
+}
+}
 
-let ArticulosCard = document.getElementById("card");
+Productos();
 
-ArticulosCard.innerHTML `
-            <div class="card col-4">
-                    <img src=../Imagenes/intel.jpg class="card-img-top" alt=Procesador>
-                          <div class="card-body">
-                          <h5 class="card-title">${Procesadores.nombre}</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                          <a href=Boton class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-`;
+Procesadores.forEach((Procesador) => {
+  document.getElementById(`Boton${Procesador.id}`).addEventListener("click",function(){
+    AgregarAlCarrito(Procesador);
+  })
+  
+});
+
+function AgregarAlCarrito(ProcesadorAComprar){
+   carrito.push(ProcesadorAComprar);
+   console.table(carrito);
+   Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Producto agregado al carrito!',
+    showConfirmButton: false,
+    timer: 1500
+  });
+   document.getElementById("TablaBody").innerHTML +=`
+      <tr>
+          <td>${ProcesadorAComprar.id}<td>
+          <td>${ProcesadorAComprar.nombre}<td>
+          <td>${ProcesadorAComprar.precio}<td>
+      <tr>
+   `;
+   let total = carrito.reduce((acumulador,prod)=> acumulador + prod.precio,0)
+   document.getElementById("total").innerText="Total a Pagar $: "+total;
+}
+
+
+const GuardoLocal = JSON.parse(localStorage,getItem("Procesadores")) || [];
+
+GuardoLocal("Procesadores", JSON,stringify(Procesador));
--- js/document.js
+++ js/document.js
@@ -1,124 +1,24 @@
-localStorage.setItem("usuario","Martin Minuto")
-localStorage.setItem("ano","2022")
+let libros=[];
 
+function ObtenerDatos(){
+  const URLGET= "https://api.itbook.store/1.0/new";
+  fetch(URLGET)
+     .then(resultado => resultado.json())
+     .then(data => {
+          console.log(data);
+          libros=data.books;
+          console.log(libros);
+          libros.forEach((libro)=>{
+            document.getElementById("Apis").innerHTML += `
+                <tr>    
+                    <td>${libro.title}</td>
+                    <td><img src=${libro.image}></td>
+                </tr>
+            `
+          })
 
-
-
-/*let Teclado = 500;
-let Mouse = 300;
-let Monitor = 600;
-let MousePad = 50;
-let Procesador = 500;
-let Gabinete = 200;
-let Auriculares = 150;
-let Ventilador = 30;
-let precio;
-
-let Productos =prompt("Ingresar Articulo");
-while (Productos != "Salir"){
-    switch (Productos){
-        case "Teclado":
-         console.log("Vale $"+ Teclado);
-         break;
-        case "Mouse":
-         console.log("Vale $"+ Mouse);
-         break;
-        case "Monitor":
-         console.log("Vale $"+ Monitor);
-         break;
-         case "Mousepad":
-         console.log("Vale $"+ MousePad);
-         break;
-         case "Procesardor":
-         console.log("Vale $"+ Procesador);
-         break;
-         case "Gabinete":
-         console.log("Vale $"+ Gabinete);
-         break;
-         case "Auriculares":
-         console.log("Vale $"+ Auriculares);
-         break;
-         case "Ventilador":
-         console.log("Vale $"+ Ventilador);
-         break;
-         default:
-            console.log("El precio no se puede calcular");
-            break;
-    }
-    Productos=prompt("Pregunta otro precio o escribi Salir");
-    
+     });
 }
 
-let VerIVAProducto=parseFloat(prompt("ingresa precio para sacar el IVA"))
+ObtenerDatos();
 
-function CalIVA (precio){
-    return precio *0.21;
-}
-
-let iva = CalIVA(VerIVAProducto);
-console.log("El IVA es $"+iva);
-
-
-function SumarPrecios(precio1, precio2){
-    let resultado= precio1+precio2
-    console.log("El Total es "+resultado);
-}
-
-
- Procesadores.forEach((Procesadores) => console.log(Procesadores.nombre, Procesadores.precio));
-
-    let BuscarProcesador = prompt("Que producto estas buscando?")
-    const resultado = Procesadores.filter((Procesador)=> Procesador.marca.includes("Intel"))
-    if (resultado != undefined){
-       console.log(resultado);
-    }else{
-       console.log("No se encuentra el procesador solicitado!");
-    }
-*/
-
-const Carrito = [];
-
-
-const Procesador = 
-    {
-     id:1,
-     nombre:"I3 9gen",
-     marca:"Intel",
-     precio:"$900",
-     stock:"Sin stock", // De momento
-     imagen:'./imagenes/intel.jpg'
-    }
-
- 
- 
- let ArticulosCard = document.getElementById('card');
- 
- ArticulosCard.innerHTML = `
-             <div class="card col-4">
-                     <img src=../Imagenes/intel.jpg class="card-img-top" alt=Procesador>
-                           <div class="card-body">
-                           <h5 class="card-title">${Procesador.nombre}</h5>
-                           <p class="card-text">${Procesador.precio}</p>
-                           <button id="Boton" class="btn">Comprar</button> 
-                     </div>
-                 </div>
- `;
-
-
-
- let Boton = document.getElementById("Boton");
-
-
- Boton.addEventListener("click",AgregarAlCarrito);
-
- function AgregarAlCarrito(){
-    alert("Producto agregado al carrito");
-    Carrito.push(Procesador);
-    console.table(Carrito);
- }
-
-
-
-
- const PrecioTotal = Procesador.reduce((sumar,Procesador)=> sumar + Procesador.precio,0);
-console.log("El total a abonar es $ "+ PrecioTotal);
